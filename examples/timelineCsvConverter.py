36c5422ea2fa9ee97c6d2a546e4f326254336695
jasshauer <88253348+jasshauer@users.noreply.github.com>
2021-11-05 01:43:39 +0100

    Update timelineCsvConverter.py
    add "Bonuszahlung" as "Einlage"

--- examples/timelineCsvConverter.py
+++ examples/timelineCsvConverter.py
@@ -93,6 +93,12 @@ with open("myTransactions.csv", "w") as f:
                     date, "Einlage", "", "", event["cashChangeAmount"], "", "", ""
                 )
             )
+        elif title == "Bonuszahlung":
+            f.write(
+                "{0};{1};{2};{3};{4};{5};{6};{7}\n".format(
+                    date, "Einlage", "", "", event["cashChangeAmount"], "", "", ""
+                )
+            )
         elif title == "Auszahlung":
             f.write(
                 "{0};{1};{2};{3};{4};{5};{6};{7}\n".format(
